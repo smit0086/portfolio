d9ce900ebb45c78c80d97c18b0c3af2995dda67f
Smit Patel <smit.patel@dal.ca>
2024-05-26 20:27:05 -0300

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'purple': '#6E06F2',
+			'white': '#FFFFFF',
+			'grey': '#4E525A',
+			'light-grey': '#B8B8B8',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
